c2d4b6910043695e5df5ece3ce719b774e21b537
Daniele Orler <daniele.orler@gmail.com>
2016-07-26 00:19:15 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# [START app]
+import logging
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Ciao Claudia!'
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+# [END app]
