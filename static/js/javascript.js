19ed9672dcdbcecc224c02565f7e2ebfc3d16d55
claudia.zanetel <claudia.zanetel@gmail.com>
2016-10-13 20:58:12 +0200

    new structure for choose groups

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,6 +1,20 @@
+function Button_fuction(){
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+}
+
 function create_groups() {
-	  
-	  var file = document.getElementById("file").value;
+	    
+	  //}​​
+	  var file = document.getElementById("files").value;
 	  var n_group = document.getElementById("num_groups").value;
 	  if(document.getElementById('type_aged').checked) {
 		  var type = 1
@@ -8,6 +22,8 @@ function create_groups() {
 	  else if(document.getElementById('type_mix').checked) {
 			var type = 2
 	  }
-      //document.write(type + "  " + n_group );
+      
+	  
+	  //document.write(type + "  " + n_group );
       location.href = "/groups/" + file+ "/" +type+"/"+n_group;
 }
