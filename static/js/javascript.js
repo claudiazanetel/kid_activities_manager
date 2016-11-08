4d1e702b15a4f8d7806ec183a29a42a6ee5f3f79
claudia.zanetel <claudia.zanetel@gmail.com>
2016-11-08 21:47:08 +0100

    change form that it creates the groups

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -15,12 +15,19 @@ function create_groups() {
 	    
 	  var file = document.getElementById("files").value;
 	  var n_group = document.getElementById("num_groups").value;
-	  if(document.getElementById('type_aged').checked) {
+	  var t_group = document.getElementById("type_group").value;
+	  if (t_group =='aged'){
 		  var type = 1
 	  }
-	  else if(document.getElementById('type_mix').checked) {
-			var type = 2
+	  else if (t_group =='mix'){
+		  var type = 2 
 	  }
+	  //if(document.getElementById('type_aged').checked) {
+		  //var type = 1
+	  //}
+	  //else if(document.getElementById('type_mix').checked) {
+			//var type = 2
+	  //}
       
 	  
 	  //document.write(type + "  " + n_group );
