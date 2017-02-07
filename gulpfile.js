ef354005270b04420e59add293238ef6ce2ed2a2
claudia.zanetel <claudia.zanetel@gmail.com>
2017-02-07 18:21:59 +0100

    added minify_css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var cleanCSS = require('gulp-clean-css');
 
 
 //Default task
-gulp.task('default', ['bower']);  
+gulp.task('default', ['bower', 'minify-css']);  
 
 gulp.task('bower', function() {
- return bower('./static/components')
+ return bower('./static/dist/components')
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('./static/css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./static/dist/css'));
 });
