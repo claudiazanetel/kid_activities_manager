d08d667f0890a55766091bb0c5ae9f17ed5a5372
claudia.zanetel <claudia.zanetel@gmail.com>
2017-02-07 13:04:49 +0100

    added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var bower = require('gulp-bower');
 
 
 //Default task
-gulp.task('default', function() {
+gulp.task('default', ['bower']);  
+
+gulp.task('bower', function() {
+ return bower('./static/components')
 });
